4489b1b40425bb452ba3bfd36a1b4c5c8284ce25
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2023-11-26 18:13:16 -0300

    Add files via upload

---
+++ atividade3/atv2/src/main/java/com/mycompany/atividade_calculadora/Calculadora.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.atividade_calculadora;
+
+/**
+ *
+ * @author shayr
+ */
+class Calculadora {
+
+    double calcularPosfixa(String expressaoPosfixa) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String expressaoPosfixaParaInfixa(String expressaoPosfixa) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    double calcularExpressao(String expressaoPosfixa, String posfixa) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
