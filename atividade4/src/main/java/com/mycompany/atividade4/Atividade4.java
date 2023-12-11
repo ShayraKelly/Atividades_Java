c0c01b4b92592896df2d59b78ca5b8bfee9d7f79
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2023-12-11 08:44:41 -0300

    Update

--- atividade4/src/main/java/com/mycompany/atividade4/Atividade4.java
+++ atividade4/src/main/java/com/mycompany/atividade4/Atividade4.java
@@ -4,13 +4,38 @@
 
 package com.mycompany.atividade4;
 
+import java.util.Arrays;
+
 /**
  *
  * @author shayr
  */
-public class Atividade4 {
 
+public class Atividade4 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Lista fl = new Lista();
+        int numeros[] = new int[1000];
+        int x, min = -9999, max = 9999;
+        int maior = -9999, menor = 9999;
+
+        for (x = 0; x < 1000; x++) {
+            numeros[x] = (int) (Math.random() * (max - min + 1) + min);
+        }
+        
+        System.out.println("Números: " + Arrays.toString(numeros) + "\n");
+
+        for (x = 0; x < 1000; x++) {
+            if (maior <= numeros[x]) {
+                fl.addFinal(numeros[x]);
+                maior = numeros[x];
+            } else if (menor >= numeros[x]) {
+                fl.addInicio(numeros[x]);
+                menor = numeros[x];
+            } else {
+                fl.addMeio(fl.buscaPosicao(numeros[x]), numeros[x]);
+            }
+        }
+
+        fl.imprimirLista();
     }
 }
