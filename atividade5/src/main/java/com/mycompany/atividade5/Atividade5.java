ce54ba864a4a5aebc972b589054a93b46a236d9e
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2023-12-11 09:12:21 -0300

    Update

--- atividade5/src/main/java/com/mycompany/atividade5/Atividade5.java
+++ atividade5/src/main/java/com/mycompany/atividade5/Atividade5.java
@@ -8,9 +8,41 @@ package com.mycompany.atividade5;
  *
  * @author shayr
  */
-public class Atividade5 {
 
+import java.util.Scanner;
+
+public class Atividade5 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a expressão: ");
+        String expressao = scanner.nextLine();
+
+        if (verificarExpressao(expressao)) {
+            System.out.println("A expressão está correta!");
+        } else {
+            System.out.println("A expressão está incorreta!");
+        }
+    }
+
+    static boolean verificarExpressao(String expressao) {
+        Pilha pilha = new Pilha();
+
+        for (char caractere : expressao.toCharArray()) {
+            if (caractere == '(' || caractere == '[' || caractere == '{') {
+                pilha.empilhar(caractere);
+            } else if (caractere == ')' || caractere == ']' || caractere == '}') {
+                if (pilha.estaVazia() || !parametrosCorrespondentes(pilha.desempilhar(), caractere)) {
+                    return false;
+                }
+            }
+        }
+
+        return pilha.estaVazia();
+    }
+
+    static boolean parametrosCorrespondentes(char abre, char fecha) {
+        return (abre == '(' && fecha == ')') ||
+               (abre == '[' && fecha == ']') ||
+               (abre == '{' && fecha == '}');
     }
 }
